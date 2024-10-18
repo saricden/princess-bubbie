ba27b5d538336d67a8b468d420cd1398a90c7a99
Kirk Morris <worker.kirk@gmail.com>
2024-10-17 18:29:48 -0700

    Adds PWA-ness.

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Princess Bubbie',
+        short_name: 'Princess Bubbie',
+        description: 'A game about the legendary princess bubbie.',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
