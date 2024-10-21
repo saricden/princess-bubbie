7f14875ff92ac49fd84f0aaa40d614d635c3ef75
Kirk Morris <worker.kirk@gmail.com>
2024-10-20 23:53:51 -0700

    Adds preloader bar.

--- scenes/Boot.js
+++ scenes/Boot.js
@@ -6,36 +6,72 @@ export default class Boot extends Scene {
 	}
 
 	preload() {
-    // Load the Tiled tilemap and tileset image
-    this.load.tilemapTiledJSON('map-test', '/maps/test.json');
-    this.load.image('tiles', '/maps/tiles.png');
+	    const width = this.cameras.main.width;
+	    const height = this.cameras.main.height;
+	    const progressBar = this.add.graphics();
+	    const progressBox = this.add.graphics();
+	    
+	    progressBox.fillStyle(0x222222, 0.8); // dark grey
+	    progressBox.fillRect(width / 4, height / 2 - 25, width / 2, 50);
 
-    // Sprites
-    this.load.aseprite('bubbie', '/sprites/bubbie.png', '/sprites/bubbie.json');
-    this.load.aseprite('gargoyle', '/sprites/gargoyle.png', '/sprites/gargoyle.json');
-    this.load.image('sus', '/sprites/sus.png');
-    this.load.image('boo', '/sprites/boo.png');
-    this.load.aseprite('ghost', '/sprites/ghost.png', '/sprites/ghost.json');
+	    // Display percentage text
+	    const percentText = this.make.text({
+	      x: width / 2,
+	      y: height / 2,
+	      text: '0%',
+	      style: {
+	        font: '18px monospace',
+	        fill: '#ffffff'
+	      }
+	    });
+	    percentText.setOrigin(0.5, 0.5);
 
-    // UI
-    this.load.aseprite('ui-heart', '/sprites/ui/heart.png', '/sprites/ui/heart.json');
+	    // Load the Tiled tilemap and tileset image
+	    this.load.tilemapTiledJSON('map-test', '/maps/test.json');
+	    this.load.image('tiles', '/maps/tiles.png');
 
-    // Sound fx
-    this.load.audio('sfx-step1', '/sfx/metal_steps_02.mp3');
-    this.load.audio('sfx-step2', '/sfx/metal_steps_03.mp3');
-    this.load.audio('sfx-step3', '/sfx/metal_steps_12.mp3');
-    this.load.audio('sfx-step4', '/sfx/metal_steps_15.mp3');
-    this.load.audio('sfx-step5', '/sfx/metal_steps_18.mp3');
-    this.load.audio('sfx-hit1', '/sfx/hit05.mp3');
-    this.load.audio('sfx-hit2', '/sfx/hit17.mp3');
-    this.load.audio('sfx-hit3', '/sfx/hit24.mp3');
-    this.load.audio('sfx-hit4', '/sfx/hit26.mp3');
-    this.load.audio('sfx-hit5', '/sfx/hit35.mp3');
-    this.load.audio('sfx-kaboom', '/sfx/kaboom.mp3');
+	    // Sprites
+	    this.load.aseprite('bubbie', '/sprites/bubbie.png', '/sprites/bubbie.json');
+	    this.load.aseprite('gargoyle', '/sprites/gargoyle.png', '/sprites/gargoyle.json');
+	    this.load.image('sus', '/sprites/sus.png');
+	    this.load.image('boo', '/sprites/boo.png');
+	    this.load.aseprite('ghost', '/sprites/ghost.png', '/sprites/ghost.json');
 
-    // FX
-    this.load.image('pixel', '/fx/pixel.png');
-  }
+	    // UI
+	    this.load.aseprite('ui-heart', '/sprites/ui/heart.png', '/sprites/ui/heart.json');
+
+	    // Sound fx
+	    this.load.audio('sfx-step1', '/sfx/metal_steps_02.mp3');
+	    this.load.audio('sfx-step2', '/sfx/metal_steps_03.mp3');
+	    this.load.audio('sfx-step3', '/sfx/metal_steps_12.mp3');
+	    this.load.audio('sfx-step4', '/sfx/metal_steps_15.mp3');
+	    this.load.audio('sfx-step5', '/sfx/metal_steps_18.mp3');
+	    this.load.audio('sfx-hit1', '/sfx/hit05.mp3');
+	    this.load.audio('sfx-hit2', '/sfx/hit17.mp3');
+	    this.load.audio('sfx-hit3', '/sfx/hit24.mp3');
+	    this.load.audio('sfx-hit4', '/sfx/hit26.mp3');
+	    this.load.audio('sfx-hit5', '/sfx/hit35.mp3');
+	    this.load.audio('sfx-kaboom', '/sfx/kaboom.mp3');
+
+	    // FX
+	    this.load.image('pixel', '/fx/pixel.png');
+
+	    // Update progress bar based on loading progress
+	    this.load.on('progress', (value) => {
+	      percentText.setText(parseInt(value * 100) + '%');
+	      
+	      progressBar.clear();
+	      progressBar.fillStyle(0xffffff, 1); // white
+	      progressBar.fillRect(width / 4 + 10, height / 2 - 15, (width / 2 - 20) * value, 30);
+	    });
+
+	    // Clean up when load is complete
+	    this.load.on('complete', () => {
+	      progressBar.destroy();
+	      progressBox.destroy();
+	      percentText.destroy();
+	    });
+	  }
 
 	create() {
 		const {width: w, height: h} = this.game.scale;
@@ -44,7 +80,10 @@ export default class Boot extends Scene {
 		this.anims.createFromAseprite('gargoyle');
 		this.anims.createFromAseprite('ghost');
 
-		this.add.text(w / 2, h / 2, 'Click or tap to Play').setOrigin(0.5);
+		this.add.text(w / 2, h / 2, 'Click to Play', {
+	        font: '62px monospace',
+	        fill: '#ffffff'
+		}).setOrigin(0.5);
 
 		this.input.once('pointerdown', () => {
 			this.scene.start('scene-test');
